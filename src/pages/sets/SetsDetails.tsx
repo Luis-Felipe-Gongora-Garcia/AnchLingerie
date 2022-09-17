7bcbe719743ea933c142ee01b30e9a7cb0886c38
Luis Felipe <lfelipeggarcia@gmail.com>
2022-09-17 12:53:53 -0300

    feat: adjusts SetsDetails

--- src/pages/sets/SetsDetails.tsx
+++ src/pages/sets/SetsDetails.tsx
@@ -6,13 +6,16 @@ import {
   ConjuntosService,
   IDetailSets,
 } from '../../shared/services/api/conjuntos/ConjuntosService';
+import { Box, Grid, Paper, Typography } from '@mui/material';
+import SophiaB from '../../assets/images/conjuntoSophiaBranco.jpg';
 
 export const SetsDetails: React.FC = () => {
   const { id = '' } = useParams<'id'>();
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
-  const [set, setSet] = useState<IDetailSets[]>([]);
+  const [descricao, setDescricao] = useState('');
   const [name, setName] = useState('');
+  const [tamanhos, setTamanhos] = useState('');
 
   useEffect(() => {
     setIsLoading(true);
@@ -25,6 +28,8 @@ export const SetsDetails: React.FC = () => {
       }
       console.log(result);
       setName(result.nome);
+      setDescricao(result.descricao);
+      setTamanhos(result.tamanhos);
     });
   }, [id]);
 
@@ -33,7 +38,52 @@ export const SetsDetails: React.FC = () => {
       title={name}
       toolbar={<ToolDetail onClickBack={() => navigate('/conjuntos')} />}
     >
-      <p>Detalhes {id}</p>
+      <Box
+        display='flex'
+        width='auto'
+        height='auto'
+        flex={1}
+        justifyContent='center'
+        alignItems='center'
+        margin={1}
+        component={Paper}
+      >
+        <Grid container spacing={2}>
+          <Grid item>
+            <Box m={3} component='div'>
+              <img height='700' src={SophiaB} />
+            </Box>
+          </Grid>
+          <Grid
+            display='flex'
+            justifyContent='center'
+            alignItems='center'
+            flex={1}
+            item
+            xs={4}
+          >
+            <Box
+              m={3}
+              component='div'
+              display='flex'
+              flexDirection='column'
+              justifyContent='center'
+              alignItems='center'
+              flex={1}
+            >
+              <Typography gutterBottom variant='h2'>
+                {name}
+              </Typography>
+              <Typography gutterBottom variant='body1'>
+                {descricao}
+              </Typography>
+              <Typography gutterBottom variant='body2'>
+                Tamanhos disponiveis: {tamanhos}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </LayoutBasePage>
   );
 };
